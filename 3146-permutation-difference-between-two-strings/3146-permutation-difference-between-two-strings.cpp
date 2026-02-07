d49cda7b8965d0b265401b23a84daff8905ae29a
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-07 22:09:11 +0530

    Time: 0 ms (100%), Space: 9.4 MB (41.81%) - LeetHub

---
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int permutationDiff(int s_freq[], int t_freq[]) {
+        int diff = 0;
+        for (int i = 0; i < 26; i++) {
+            diff += abs(s_freq[i] - t_freq[i]);
+        }
+        return diff;
+    }
+    int findPermutationDifference(string s, string t) {
+        int diff = 0;
+        int len = s.length();
+        int s_freq[26] = {0};
+        int t_freq[26] = {0};
+        for (int i = 0; i < len; i++) {
+            s_freq[s[i] - 'a'] = i;
+            t_freq[t[i] - 'a'] = i;
+        }
+        return permutationDiff(s_freq, t_freq);
+    }
+};
