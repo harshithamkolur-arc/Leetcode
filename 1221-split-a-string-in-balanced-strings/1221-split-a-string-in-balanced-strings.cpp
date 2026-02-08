429b49ee8b3ee5f22d20fef86a52356df568b5b6
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-08 22:24:04 +0530

    Time: 1 ms (5.15%), Space: 8.4 MB (54.68%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans = 0;
+        int count_R = 0;
+        int count_L = 0;
+        int j = 0;
+        while (j < s.length()) {
+            if (s[j] == 'R') {
+                count_R++;
+                j++;
+            } else {
+                count_L++;
+                j++;
+            }
+            if (count_R == count_L) {
+                cout<<"Hi"<<endl;
+                ans++;
+                count_R = 0;
+                count_L = 0;
+            }
+        }
+        return ans;
+    }
+};
