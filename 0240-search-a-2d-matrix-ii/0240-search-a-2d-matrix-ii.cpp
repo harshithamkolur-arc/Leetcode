bf25383610591d7157fa682582ddc8e5dd690fb7
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-10 22:26:16 +0530

    Time: 54 ms (54.08%), Space: 18.7 MB (37.02%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row_sz = matrix.size();
+        int col_sz = matrix[0].size();
+        int row = 0;
+        int col = col_sz - 1;
+        while (col >= 0 && row < row_sz) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else if (matrix[row][col] < target) {
+                row++;
+            }
+        }
+        return false;
+    }
+};
