ccf9d9828173abb83a523ff44f243c786a875fd6
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-10 23:14:21 +0530

    Time: 65 ms (19.57%), Space: 18.8 MB (37.02%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    bool BinarySearch(vector<vector<int>>& matrix, int target,int row,int col) {
+        int sz = col;
+        int st = 0;
+        int end = sz;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] < target) {
+                st = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row_sz = matrix.size();
         int col_sz = matrix[0].size();
@@ -10,6 +26,12 @@ public:
                 return true;
             } else if (matrix[row][col] > target) {
                 col--;
+                if (target >= matrix[row][0] && target <= matrix[row][col]) {
+                    BinarySearch(matrix,target,row,col);
+                }
+                else{
+                    row++;
+                }
             } else if (matrix[row][col] < target) {
                 row++;
             }
@@ -17,4 +39,3 @@ public:
         return false;
     }
 };
-
