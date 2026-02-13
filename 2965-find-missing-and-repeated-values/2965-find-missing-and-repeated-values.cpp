dc9fcce466c19411ffa1e4a99dcf1f8588950204
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-13 22:29:50 +0530

    Time: 21 ms (16.57%), Space: 31.6 MB (37.81%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans;
         unordered_set<int> s;
         int grid_sum = 0, req_sum = 0;
-        int a, b;
+        int a,b;
         int n = grid.size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -16,7 +16,7 @@ public:
                 s.insert(grid[i][j]);
             }
         }
-        req_sum = (n * n) * (n * n + 1) / 2;
+        req_sum = ((n*n)*((n*n)+1))/2;
         b = req_sum - grid_sum + a;
         ans.push_back(b);
         return ans;
