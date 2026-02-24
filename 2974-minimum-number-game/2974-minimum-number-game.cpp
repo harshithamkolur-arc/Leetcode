1afa1fefc7edc8a66ed968c5f59f259b77a2218b
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-24 22:30:52 +0530

    Time: 0 ms (100%), Space: 28.2 MB (62.17%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    vector<int> game(vector<int>& nums, int i, int n, vector<int>& ans) {
-        int idx = i + 1;
+    vector<int> game(vector<int>& nums, int i, int n) {
         if (i == n - 2) {
-            ans.push_back(nums[idx]);
-            ans.push_back(nums[idx - 1]);
-            return ans;
+            swap(nums[i],nums[i+1]);
+            return nums;
         }
-        ans.push_back(nums[idx]);
-        ans.push_back(nums[idx - 1]);
-        return game(nums, i + 2, n, ans);
+        swap(nums[i],nums[i+1]);
+        return game(nums, i + 2, n);
     }
     vector<int> numberGame(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> ans;
-        return game(nums, 0, n, ans);
+        return game(nums, 0, n);
     }
 };
