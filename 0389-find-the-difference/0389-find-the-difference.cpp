9ce851939e337ffdb75e951224d995a6d73dd72c
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-24 23:20:59 +0530

    Time: 3 ms (12.96%), Space: 9.6 MB (8.3%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
+        // Use XOR of optimal solution
         unordered_map<char, int> freq;
         for (char c : s) {
             freq[c]++;
         }
         for (char c : t) {
             freq[c]--;
-            if(freq[c]<0){
+            if (freq[c] < 0) {
                 return c;
             }
         }
