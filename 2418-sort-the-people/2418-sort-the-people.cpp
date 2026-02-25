a5e8b0c92fc582526cc308aa507597331c2b0bf5
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-02-25 22:41:15 +0530

    Time: 165 ms (12.56%), Space: 24.1 MB (90.6%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    vector<string> arrange(vector<string>& names, vector<int>& heights, int idx,
-                           int n) {
+    void arrange(vector<string>& names, vector<int>& heights, int idx, int n) {
         if (n == 0) {
-            return names;
+            return;
         }
         if (idx < n) {
             if (heights[idx] < heights[idx + 1]) {
                 swap(heights[idx], heights[idx + 1]);
                 swap(names[idx], names[idx + 1]);
             }
-            return arrange(names, heights, idx + 1, n);
+            arrange(names, heights, idx + 1, n);
+        } else {
+            arrange(names, heights, 0, n - 1);
         }
-        return arrange(names, heights, 0, n - 1);
     }
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n = names.size() - 1;
-        return arrange(names, heights, 0, n);
+        arrange(names, heights, 0, n);
+        return names;
     }
 };
