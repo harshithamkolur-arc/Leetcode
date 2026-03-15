3dde50745ecd35476b9262f647409ad12657efb9
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-03-15 22:37:42 +0530

    Time: 11 ms (8.66%), Space: 26.5 MB (21.43%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        unordered_map<int,int>map;
+        unordered_set<int>set;
         vector<int> ans;
         for(int val: nums){
-            if(map.find(val) == map.end()){
-                map[val] = 1;
+            if(set.find(val) == set.end()){
+                set.insert(val);
             }
             else{
                 ans.push_back(val);
