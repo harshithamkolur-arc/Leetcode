8372f971644d2e313e6c66e0baeb25cd84f8df82
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-07-27 10:44:01 +0530

    Time: 5 ms (11.26%), Space: 45.2 MB (7.67%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0;
+        while (nums[i] < k) {
+            i++;
+        }
+        return i;
+    }
+}
