7a4159d0698500ebff6e0d328481a00147a19c2c
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-08-18 17:27:30 +0530

    Time: 3 ms (63.51%), Space: 44.7 MB (74.74%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,40 +1,34 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        if(k==1){
-            Integer largest = Integer.MIN_VALUE;
-            for(Map.Entry<Integer,Integer>entry : map.entrySet()){
-                if(entry.getValue() == 1){
-                    largest = Math.max(largest,entry.getKey());
+        if (k == 1) {
+            int largest = Integer.MIN_VALUE;
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == 1) {
+                    largest = Math.max(largest, entry.getKey());
                 }
             }
             return largest == Integer.MIN_VALUE ? -1 : largest;
-        }
-        else if(k == nums.length){
+        } else if (k == nums.length) {
             int largest = Integer.MIN_VALUE;
-            for(int val : nums){
-                largest = Math.max(largest,val);
+            for (int val : nums) {
+                largest = Math.max(largest, val);
             }
             return largest;
-        }
-        else{
-           // System.out.println(map.get(nums[0])+" "+map.get(nums[nums.length - 1]));
-            if(map.get(nums[0]) > 1 && map.get(nums[nums.length - 1]) > 1){
-                
-                return -1;
-            }
-            else if(map.get(nums[0]) > 1){
-                return nums[nums.length - 1];
-            }
-            else if(map.get(nums[nums.length - 1]) > 1){
-                return nums[0];
-            }
-            else{
-                return Math.max(nums[0],nums[nums.length - 1]);
-            }
+        } else {
+            int first = nums[0];
+            int last = nums[nums.length-1];
+
+            boolean firstfreq = map.get(first) == 1;
+            boolean lastfreq = map.get(last) == 1;
+
+            if(!firstfreq && !lastfreq) return -1;
+            if(firstfreq && lastfreq) return Math.max(first,last);
+            return firstfreq ? first : last;
+            
         }
     }
 }
