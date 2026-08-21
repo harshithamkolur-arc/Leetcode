f8df500a000b8c0a8d9c0aa1c4505f7e767fd658
harshithamkolur-arc <harshithamkolur@gmail.com>
2026-08-21 11:04:48 +0530

    Time: 1 ms (40.41%), Space: 42.2 MB (99.29%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -3,8 +3,9 @@ class Solution {
         String[] words = s.split(" ");
         String[] sorted = new String[words.length];
         for (int i = 0; i < words.length; i++) {
-            int idx = (int) (words[i].charAt(words[i].length() - 1) - '0');
-            sorted[idx - 1] = words[i].substring(0, words[i].length() - 1);
+            String currWord = words[i];
+            int idx = Character.getNumericValue(currWord.charAt(currWord.length() - 1));
+            sorted[idx - 1] = currWord.substring(0, currWord.length() - 1);
         }
         String sentence = String.join(" ", sorted);
         return sentence;
